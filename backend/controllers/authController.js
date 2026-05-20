e4e8c0f160cbbf6fc58711644c896b4a1a14bd33
Saurabh Kumar Singh <imsaurabhsinha51@gmail.com>
2026-05-20 22:30:22 +0530

    auth controller modified

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,22 +1,22 @@
 import User from "../models/userModel.js";
 import validator from "validator";
 import bcrypt from "bcryptjs";
-import  genToken  from "../config/token.js";
+import genToken from "../config/token.js";
 import sendMail from "../config/sendMail.js";
 
 
-export const signup = async(req, res) => {
-    try{
-        const {name, email, password, role} = req.body;
-        let existUser = await User.findOne({email});
-        if(existUser){
-            return res.status(400).json({message: "User already exists"});
+export const signup = async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
+        let existUser = await User.findOne({ email });
+        if (existUser) {
+            return res.status(400).json({ message: "User already exists" });
         }
-        if(!validator.isEmail(email)){
-            return res.status(400).json({message: "Invalid email"});
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({ message: "Invalid email" });
         }
-        if(password.length < 8){
-            return res.status(400).json({message: "Password must be at least 8 characters"});
+        if (password.length < 8) {
+            return res.status(400).json({ message: "Password must be at least 8 characters" });
         }
         let hashPassword = await bcrypt.hash(password, 10);
         const user = await User.create({
@@ -29,54 +29,58 @@ export const signup = async(req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: false,
-             sameSite: "None",
-            maxAge: 7*24*60*60*1000,
+            sameSite: "None",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         })
         return res.status(201).json(user);
-    }catch(error){
-        return res.status(500).json({message: `Signup error ${error}`});
+    } catch (error) {
+        return res.status(500).json({ message: `Signup error ${error}` });
     }
 }
 
-export const login = async(req, res) => {
-    try{
-        const {email, password} = req.body;
-        const user = await User.findOne({email});
-        if(!user){
-            return res.status(404).json({message: "User does not exist"});
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User does not exist" });
         }
         const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch){
-            return res.status(400).json({message: "Invalid credentials"});
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
         }
         let token = await genToken(user._id);
         res.cookie("token", token, {
             httpOnly: true,
             secure: false,
-             sameSite: "None",
-            maxAge: 7*24*60*60*1000,
+            sameSite: "None",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         })
         return res.status(200).json(user);
-    }catch(error){
-        return res.status(500).json({message: `Login error ${error}`});
+    } catch (error) {
+        return res.status(500).json({ message: `Login error ${error}` });
     }
 }
 
-export const logout = async(req, res) => {
-    try{
-        await res.clearCookie("token");
-        return res.status(200).json({message: "Logged out successfully"});
-    }catch(error){
-        return res.status(500).json({message: `Logout error ${error}`});
+export const logout = async (req, res) => {
+    try {
+        await res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None",
+        });
+        return res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        return res.status(500).json({ message: `Logout error ${error}` });
     }
 }
 
 export const sendOTP = async (req, res) => {
-    try{
-        const {email} = req.body;
-        const  user = await User.findOne({email});
-        if(!user){
-            return res.status(404).json({message: "User does not exist"});
+    try {
+        const { email } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User does not exist" });
         }
         const otp = Math.floor(1000 + Math.random() * 9000).toString();
         user.resetOtp = otp;
@@ -84,52 +88,52 @@ export const sendOTP = async (req, res) => {
         user.isOtpVerified = false;
         await user.save();
         await sendMail(email, otp);
-        return res.status(200).json({message:"otp send successfully"})
-    }catch(err){
-         return res.status(500).json({message: `send otp error ${err}`});
+        return res.status(200).json({ message: "otp send successfully" })
+    } catch (err) {
+        return res.status(500).json({ message: `send otp error ${err}` });
     }
 }
 
 export const VerifyOTP = async (req, res) => {
-    try{
-        const {email, otp} = req.body;
-        const user = await User.findOne({email});
-        if(!user || user.resetOtp != otp || user.otpExpires  < Date.now()){
-            return res.status(404).json({message: "User does not exist"});
+    try {
+        const { email, otp } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || user.resetOtp != otp || user.otpExpires < Date.now()) {
+            return res.status(404).json({ message: "User does not exist" });
         }
-         user.resetOtp = undefined;
+        user.resetOtp = undefined;
         user.otpExpires = undefined;
         user.isOtpVerified = true;
 
         await user.save();
-       return res.status(200).json({message:"otp verified successfully"})
-    }catch(err){
-        return res.status(500).json({message: ` otp verification error ${err}`});
+        return res.status(200).json({ message: "otp verified successfully" })
+    } catch (err) {
+        return res.status(500).json({ message: ` otp verification error ${err}` });
     }
 }
 
 export const resetPassword = async (req, res) => {
-    try{
-        const {email, password} = req.body;
-        const user = await User.findOne({email});
-        if(!user || !user.isOtpVerified){
-            return res.status(404).json({message: "Otp verification required"});
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !user.isOtpVerified) {
+            return res.status(404).json({ message: "Otp verification required" });
         }
         const hashPassword = await bcrypt.hash(password, 10);
         user.password = hashPassword;
         user.isOtpVerified = false;
         await user.save();
-        return res.status(200).json({message:"reset password successfully"})
-    }catch(err){
-        return res.status(500).json({message: `reset password error ${err}`});
+        return res.status(200).json({ message: "reset password successfully" })
+    } catch (err) {
+        return res.status(500).json({ message: `reset password error ${err}` });
     }
 }
 
-export const googleAuth = async(req, res) => {
-    try{
-        const {name, email, role} = req.body;
-        const user = await User.findOne({email});
-        if(!user){
+export const googleAuth = async (req, res) => {
+    try {
+        const { name, email, role } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
             user = await User.create({
                 name,
                 email,
@@ -141,10 +145,10 @@ export const googleAuth = async(req, res) => {
             httpOnly: true,
             secure: false,
             sameSite: "None",
-            maxAge: 7*24*60*60*1000,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         })
         return res.status(200).json(user);
-    }catch(err){
-        return res.status(500).json({message: `google auth error ${err}`});
+    } catch (err) {
+        return res.status(500).json({ message: `google auth error ${err}` });
     }
 }
