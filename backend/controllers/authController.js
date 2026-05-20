e73f3f5615a66e62239deadc20b240ac53539fa0
Saurabh Kumar Singh <imsaurabhsinha51@gmail.com>
2026-05-20 20:57:42 +0530

    modified auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -53,7 +53,7 @@ export const login = async(req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: false,
-            samesite: "Strict",
+            samesite: "None",
             maxAge: 7*24*60*60*1000,
         })
         return res.status(200).json(user);
