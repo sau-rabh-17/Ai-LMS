64b778e9dc35f51de4bb0f9f0406af49e1b592df
Saurabh Kumar Singh <imsaurabhsinha51@gmail.com>
2026-05-20 20:45:46 +0530

    modified publishred course

--- frontend/src/customHooks/getPublishedCourse.js
+++ frontend/src/customHooks/getPublishedCourse.js
@@ -1,26 +1,49 @@
-import axios from 'axios'
-import React from 'react'
-import { useEffect } from 'react'
-import { serverUrl } from '../App'
-import { useDispatch } from 'react-redux'
-import { setCourseData } from '../redux/courseSlice'
+import axios from "axios";
+import { useEffect } from "react";
+import { serverUrl } from "../App";
+import { useDispatch } from "react-redux";
+import { setCourseData } from "../redux/courseSlice";
 
 const getPublishedCourse = () => {
-    const dispatch = useDispatch();
-  useEffect(() => {
-    const getCourseData = async() => {
-        try{
-            const result = await axios.get(serverUrl + "/api/course/getpublished"
-                , {withCredentials: true}
-            )
-            dispatch(setCourseData(result.data));
-            console.log(result.data);
-        }catch(err){
-            console.log(err)
-        }
-    }
-    getCourseData();
-  },[])
-}
 
-export default getPublishedCourse
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        const getCourseData = async () => {
+
+            try {
+
+                const result = await axios.get(
+                    serverUrl + "/api/course/getpublished",
+                    {
+                        withCredentials: true,
+                    }
+                );
+
+                console.log(result.data);
+
+                dispatch(setCourseData(result.data));
+
+            } catch (err) {
+
+                console.log("Error fetching published courses");
+
+                console.log(err);
+
+                console.log(err.response);
+
+                console.log(err.response?.data);
+
+                console.log(err.response?.status);
+            }
+        };
+
+        getCourseData();
+
+    }, [dispatch]);
+
+    return null;
+};
+
+export default getPublishedCourse;
