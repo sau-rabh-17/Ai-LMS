b0fd0c34911ae151f87c365250de1973fabfbb0b
Saurabh Kumar Singh <imsaurabhsinha51@gmail.com>
2026-05-20 22:53:01 +0530

    modified index.js

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.set("trust proxy", 1);
 app.use(cors(
     {origin: "https://ai-lms-1-0ae3.onrender.com",
     credentials: true,}
