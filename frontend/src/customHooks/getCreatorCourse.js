e9c7f4addc8294a6f6f45cbd56d21578c6ee8ed8
Saurabh Kumar Singh <imsaurabhsinha51@gmail.com>
2026-05-20 16:42:41 +0530

    modified creator course

--- frontend/src/customHooks/getCreatorCourse.js
+++ frontend/src/customHooks/getCreatorCourse.js
@@ -1,29 +1,47 @@
-import axios from 'axios'
-import React from 'react'
-import { useEffect } from 'react'
-import { serverUrl } from '../App'
-import { useDispatch, useSelector } from 'react-redux'
-import { setCreatorCourseData } from '../redux/courseSlice'
+import axios from "axios";
+import { useEffect } from "react";
+import { serverUrl } from "../App";
+import { useDispatch, useSelector } from "react-redux";
+import { setCreatorCourseData } from "../redux/courseSlice";
 
-const getCreatorCourse = () => {
+const GetCreatorCourse = () => {
     const dispatch = useDispatch();
-    const {userData} = useSelector(state=>state.user)
-  return (
-    useEffect(() => {
-        const creatorCourse = async () => {
-            try{
-                const result = await axios.get(serverUrl + "/api/course/getcreatorcourses",
-                    {withCredentials: true}
-                )
-                console.log(result.data);
-                dispatch(setCreatorCourseData(result.data));
-            }catch(err){
-                console.log(err)
-            }
-        }
-        creatorCourse();
-    },[userData])
-  )
-}
+    const { userData } = useSelector((state) => state.user);
 
-export default getCreatorCourse
+    useEffect(() => {
+
+        const creatorCourse = async () => {
+            try {
+
+                const result = await axios.get(
+                    serverUrl + "/api/course/getcreatorcourses",
+                    {
+                        withCredentials: true,
+                    }
+                );
+
+                console.log(result.data);
+
+                dispatch(setCreatorCourseData(result.data));
+
+            } catch (err) {
+
+                console.log(err);
+
+                console.log(err.response);
+
+                console.log(err.response?.data);
+
+            }
+        };
+
+        if (userData) {
+            creatorCourse();
+        }
+
+    }, [userData]);
+
+    return null;
+};
+
+export default GetCreatorCourse;
